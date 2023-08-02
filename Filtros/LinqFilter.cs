21d9ef426bd3a2395d0516c34a3329273ea461da
guilh <guilherme.852@hotmail.com>
2023-08-02 18:14:36 -0300

    Adicionar arquivos de projeto.

---
+++ Filtros/LinqFilter.cs
@@ -0,0 +1,38 @@
+﻿using ScreenSoundAPI.Modelos;
+
+
+
+namespace ScreenSoundAPI.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach (var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+    public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero)
+    {   
+        //sempre que queremos buscar o subconjunto de uma lista, utiliza-se o WHERE
+        var artistasPorGeneroMusical = musicas.Where(musica =>
+        musica.Genero!.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo os artistas por gênero musical >>> {genero}");
+        foreach (var artista in artistasPorGeneroMusical)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+
+    }
+    public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeDoArtista)
+    {
+        var musicasDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeDoArtista)).ToList();
+        Console.WriteLine(nomeDoArtista);
+        foreach (var musica in musicasDoArtista)
+        {
+            Console.WriteLine($"-{musica.Nome}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+using ScreenSoundAPI.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(response)!;
+
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "rock");
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+
+        var musicasPreferidasDoGuilherme = new MusicasPreferidas("Guilherme");
+        musicasPreferidasDoGuilherme.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoGuilherme.AdicionarMusicasFavoritas(musicas[765]);
+        musicasPreferidasDoGuilherme.AdicionarMusicasFavoritas(musicas[898]);
+        musicasPreferidasDoGuilherme.AdicionarMusicasFavoritas(musicas[10]);
+        musicasPreferidasDoGuilherme.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasDoGuilherme.ExibirMusicasFavoritas();
+
+        musicasPreferidasDoGuilherme.GerarArquivoJson();
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
